52256b935a4ceb34f6ac31ebddfab5b1f07bd544
Dmytro Oseledko <d.oseledko@outlook.com>
2022-12-04 19:00:03 +0200

    Not understand

---
+++ clean_folder/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_namespace_packages
+
+with open('README.md','r', encoding="utf-8") as readme:
+    long_description = readme.read()
+
+setup(
+    name = 'clean_folder',
+    version = '1.0.0',
+    description = 'Sort folder code',
+    url = 'https://github.com/Demonytro/python-hw7-sort.git',
+    author = 'Dmytro Oseledko',
+    author_email = 'd.oseledko@outlook.com',
+    long_description = 'long_description',
+    classifiers = [
+        'Python version :: Python :: 3.10',
+        'Operating System :: Windows'
+    ],
+    license = 'MIT license',
+    packages = find_namespace_packages(),
+    install_requires = ['markdown', "requests", "flask"],
+    entry_points = {'console_scripts': ['clean-folder = clean_folder.clean:main']}
+)
